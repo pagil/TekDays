916dfd62196ae5f2b5fc287575fc7d92047fb86b
Viktar Yafimchyk <viktar.yafimchyk@carsales.com.au>
2015-03-03 13:16:27 +1100

    - Test the ? operation in Groovy

--- test/unit/com/tekdays/TekEventSpec.groovy
+++ test/unit/com/tekdays/TekEventSpec.groovy
@@ -18,11 +18,15 @@ class TekEventSpec extends Specification {
     void "test toString"() {
         when: "a tekEvent has a name and a city"
             def tekEvent = new TekEvent(name: name, city: city, organizer: [fullName: 'John Doe'] as TekUser)
-
+            def tekEvent2 = new TekEvent(name: name, city: city)
         then: "the toString method will combine them"
             tekEvent.toString() == expectedString
+            (tekEvent.organizer?.fullName && true) == expectedCmpRes1
+            tekEvent2.organizer == null
+            tekEvent2.organizer?.fullName == null
+            (tekEvent2.organizer?.fullName && true) == expectedCmpRes2
         where:
-            name         | city        | organizer   | expectedString
-            'Groovy One' | 'Melbourne' | 'Peter Pen' | 'Groovy One, Melbourne'
+            name         | city        | organizer   | expectedString          | expectedCmpRes1 | expectedCmpRes2
+            'Groovy One' | 'Melbourne' | 'Peter Pen' | 'Groovy One, Melbourne' | true            | false
     }
 }
